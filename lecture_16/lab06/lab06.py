c009e90acab95360ee5fa84f95e2e04b5a7d33cd
qixubin <qixubin@gmail.com>
2021-07-05 14:58:19 +0800

    lab06

---
+++ lecture_16/lab06/lab06.py
@@ -0,0 +1,96 @@
+this_file = __file__
+
+
+def make_adder_inc(a):
+    """
+    >>> adder1 = make_adder_inc(5)
+    >>> adder2 = make_adder_inc(6)
+    >>> adder1(2)
+    7
+    >>> adder1(2) # 5 + 2 + 1
+    8
+    >>> adder1(10) # 5 + 10 + 2
+    17
+    >>> [adder1(x) for x in [1, 2, 3]]
+    [9, 11, 13]
+    >>> adder2(5)
+    11
+    """
+    "*** YOUR CODE HERE ***"
+    def adder(b):
+        nonlocal a
+        a = a + 1
+        return a + b - 1
+    return adder
+
+
+def make_fib():
+    """Returns a function that returns the next Fibonacci number
+    every time it is called.
+
+    >>> fib = make_fib()
+    >>> fib()
+    0
+    >>> fib()
+    1
+    >>> fib()
+    1
+    >>> fib()
+    2
+    >>> fib()
+    3
+    >>> fib2 = make_fib()
+    >>> fib() + sum([fib2() for _ in range(5)])
+    12
+    >>> from construct_check import check
+    >>> # Do not use lists in your implementation
+    >>> check(this_file, 'wen', ['List'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    a, b, i = 0, 1, 0
+    def fib():
+        nonlocal a, b, i
+        if i == 0:
+            i = i + 1
+            return a
+        elif i == 1:
+            i = i + 1
+            return b
+        else:
+            i = i + 1
+            c = b
+            b = a + b
+            a = c
+            return b        
+    return fib
+
+
+def insert_items(lst, entry, elem):
+    """
+    >>> test_lst = [1, 5, 8, 5, 2, 3]
+    >>> new_lst = insert_items(test_lst, 5, 7)
+    >>> new_lst
+    [1, 5, 7, 8, 5, 7, 2, 3]
+    >>> large_lst = [1, 4, 8]
+    >>> large_lst2 = insert_items(large_lst, 4, 4)
+    >>> large_lst2
+    [1, 4, 4, 8]
+    >>> large_lst3 = insert_items(large_lst2, 4, 6)
+    >>> large_lst3
+    [1, 4, 6, 4, 6, 8]
+    >>> large_lst3 is large_lst
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    entry_index_array = []
+    for i in range(len(lst)):
+        if lst[i] == entry:
+            entry_index_array.append(i)
+
+    for i in range(len(entry_index_array)):
+        index = i + entry_index_array[i] + 1
+        lst.insert(index, elem)
+
+    return lst
+
